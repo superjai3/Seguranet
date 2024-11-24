af4b37a564ad384f798be048060f28fd626b3af1
Jaime Valdés <jaimevaldes71@gmail.com>
2024-11-24 01:54:31 +0100

    Cambios realizados para la presentación final

---
+++ Controllers/CotizadorController.cs
@@ -0,0 +1,13 @@
+﻿public class CotizadorAutoModel : PageModel
+{
+    [BindProperty]
+    public Cotizacion Cotizacion { get; set; } // Modelo de cotización
+
+    public void OnPost()
+    {
+        return Redirect("http://127.0.0.1:5500/CotizadorSeguros/html/index.html"); // Cambia el puerto al que estés usando
+    }
+}
+
+
+
